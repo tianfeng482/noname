806338422b2bd11ee0b4c47598a275efcca9d899
tianfeng482 <67142797+tianfeng482@users.noreply.github.com>
2020-06-19 18:56:27 +0800

    Update 620.R

--- 620.R
+++ 620.R
@@ -1,1 +1,10 @@
-
+一、
+1. 1）getwd()     2) setwd("D:/Rtest")     3) list.files()   4)       5) rm(x1);rm(x2)
+2. 1) is.character()   2) as.numeric()    3)x <- c(Rtest1.png, Rtest2.png, ...,Rtest1000.png)
+3. 1) rnorm(100, 2, 3)  2) 2) y1=x[5<=x<=10]  3) which(5<=x<=10)
+4. 1) header=True     2)  sep=';'  3) skip=2
+5. 1) x = paste("D:/Rtest",list.files('D:/Rtest'),sep="")  2)  y = matrix(NaN, nrow = 50, ncol = 10)
+       3)   for (i in 1:10) {
+            y[, i] = read.csv(file=x[i])
+          }
+6. 
