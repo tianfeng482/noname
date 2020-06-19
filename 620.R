ee8e00655403a3b52f252a16f7088485ad0a5a7a
tianfeng482 <67142797+tianfeng482@users.noreply.github.com>
2020-06-19 19:10:29 +0800

    Update 620.R

--- 620.R
+++ 620.R
@@ -1,10 +1,32 @@
 一、
 1. 1）getwd()     2) setwd("D:/Rtest")     3) list.files()   4)       5) rm(x1);rm(x2)
 2. 1) is.character()   2) as.numeric()    3)x <- c(Rtest1.png, Rtest2.png, ...,Rtest1000.png)
-3. 1) rnorm(100, 2, 3)  2) 2) y1=x[5<=x<=10]  3) which(5<=x<=10)
+3. 1) rnorm(100, 2, 3)  2) 2) y1=x[5<=x<=10]  3) which(5<=x & x<=10)
 4. 1) header=True     2)  sep=';'  3) skip=2
 5. 1) x = paste("D:/Rtest",list.files('D:/Rtest'),sep="")  2)  y = matrix(NaN, nrow = 50, ncol = 10)
        3)   for (i in 1:10) {
             y[, i] = read.csv(file=x[i])
           }
 6. 1) col = "red", lwd=2       2) xlab = "Rname", ylab = "Rscore"  3) test(x=3, y=2, "This is Rtest!")
+
+二、
+1. 
+# 编写函数 对于单点
+Rtest_f <- function(x){
+  if(1<x & x<5)
+    {return(2*x^2+1)}
+  else
+    return(3*x)
+}
+# 取x
+x = seq(-10, 10, 0.5)
+# sapply将单点函数映射到向量中
+y = sapply(x, Rtest_f)
+# 作图
+plot(x, y, type = "o")
+
+
+2. 
+
+
+
